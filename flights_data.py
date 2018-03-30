3322f129f9856c89f09cb20bc26444002ea1bfb5
Vicente Adolfo Bolea Sánchez <vicente.bolea@gmail.com>
2018-03-30 17:28:20 +0900

    Added flighs_data.py file which substitutes iris_data.py

---
+++ flights_data.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import tensorflow as tf
+
+TRAIN_PATH = "/scratch/vicente/train_clean.csv"
+TEST_PATH  = "/scratch/vicente/test_clean.csv"
+
+
+CSV_COLUMN_NAMES = ['Month','DayofMonth','DayOfWeek','CRSDepTime',
+                    'CRSArrTime','UniqueCarrier','FlightNum','TailNum',
+										'CRSElapsedTime','ArrDelay','Origin','Dest']
+SPECIES = ['DELAYED', 'NOT_DELAYED']
+
+def load_data(y_name='delayed'):
+    """Returns the iris dataset as (train_x, train_y), (test_x, test_y)."""
+
+    train = pd.read_csv(TRAIN_PATH, names=CSV_COLUMN_NAMES, header=0)
+    train_x, train_y = train, train.pop(y_name)
+
+    test = pd.read_csv(TEST_PATH, names=CSV_COLUMN_NAMES, header=0)
+    test_x, test_y = test, test.pop(y_name)
+
+    return (train_x, train_y), (test_x, test_y)
+
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the read end of the pipeline.
+    return dataset.make_one_shot_iterator().get_next()
+
+
+def eval_input_fn(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
+    features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the read end of the pipeline.
+    return dataset.make_one_shot_iterator().get_next()
+
+
+# The remainder of this file contains a simple example of a csv parser,
+#     implemented using a the `Dataset` class.
+
+# `tf.parse_csv` sets the types of the outputs to match the examples given in
+#     the `record_defaults` argument.
+CSV_TYPES = [[0, 0, 0, 0, 0, "", 0, "", 0, 0, "", ""]]
+
+def _parse_line(line):
+    # Decode the line into its fields
+    fields = tf.decode_csv(line, record_defaults=CSV_TYPES)
+
+    # Pack the result into a dictionary
+    features = dict(zip(CSV_COLUMN_NAMES, fields))
+
+    # Separate the label from the features
+    label = features.pop('delayed')
+
+    return features, label
+
+
+def csv_input_fn(csv_path, batch_size):
+    # Create a dataset containing the text lines.
+    dataset = tf.data.TextLineDataset(csv_path).skip(1)
+
+    # Parse each line.
+    dataset = dataset.map(_parse_line)
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the read end of the pipeline.
+    return dataset.make_one_shot_iterator().get_next()
